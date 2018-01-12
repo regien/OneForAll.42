0d264da514d2122d77d90eeffbe879707755445d
jraleman <jraleman@bendiburg.com>
2018-01-11 21:50:39 -0500

    Added icon path reference for system tray

--- srcs/main.js
+++ srcs/main.js
@@ -1,19 +1,22 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
+const {app, Tray, Menu, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const iconPath = path.join(__dirname, 'icon.png');
+let appIcon = null;
+
+let mainWindow = null;
+
+function systemTray () {
+  return ;
+}
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  //systemTray();
+  appIcon = new Tray(iconPath);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
