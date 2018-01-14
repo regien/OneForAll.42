8a582f87a0e072f045115c44e29528daf8c754f5
jraleman <jraleman@bendiburg.com>
2018-01-14 17:42:10 -0500

    Removed placeholder menu from tray

--- srcs/electron/tray.js
+++ srcs/electron/tray.js
@@ -1,28 +1,12 @@
 const {Tray, Menu} = require('electron');
-const path = require('path')
+const path = require('path');
+const icon = '../app/img/icon.png';
+const iconPath = path.join(__dirname, icon);
 
 module.exports = {
   systemTray: function (win) {
-    const iconPath = path.join(__dirname, '../app/img/icon.png');
     var appIcon = new Tray(iconPath);
     var contextMenu = Menu.buildFromTemplate([
-      {
-        label: 'Item1',
-        type: 'radio',
-        icon: iconPath
-      },
-      {
-        label: 'Item2',
-        submenu: [
-          { label: 'submenu1' },
-          { label: 'submenu2' }
-        ]
-      },
-      {
-        label: 'Item3',
-        type: 'radio',
-        checked: true
-      },
       {
         label: 'Toggle DevTools',
         accelerator: 'Alt+Command+I',
@@ -36,7 +20,6 @@ module.exports = {
         selector: 'terminate:',
       }
     ]);
-
     appIcon.setToolTip('This is my application.');
     appIcon.setContextMenu(contextMenu);
     return (appIcon);
